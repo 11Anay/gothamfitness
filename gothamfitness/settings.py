6cd99b8021e9b462966a907b67a6b737ebf328e4
11anay <1977anay@gmail.com>
2024-02-09 21:29:19 +0530

    added ip

--- gothamfitness/settings.py
+++ gothamfitness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f7%j6^4vlmh-0s)t4(&o_-u8+a(%lxnqg*eobpnuas@f2yzvhw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.127.144.110']
 
 
 # Application definition
